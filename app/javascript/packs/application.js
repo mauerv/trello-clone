eb6f2eb1fee4ed451722eed3199b44344ff90a94
mauerv <maurovelazquez1@gmail.com>
2019-06-04 14:46:43 -0300

    Initial UI with Vue.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,16 @@
-console.log('Hello World from Webpacker')
+import Vue from 'vue/dist/vue.esm'
+import App from '../app.vue'
+
+document.addEventListener('turbolinks:load', function() {
+	var element = document.querySelector('#boards')
+	if (element != undefined) {
+		const app = new Vue({
+			el: element,
+			data: {
+				lists: JSON.parse(element.dataset.lists)
+			},
+			template: `<App :original_lists='lists' />`,
+			components: { App }
+		})
+	}
+})
