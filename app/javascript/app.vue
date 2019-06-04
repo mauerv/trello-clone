5a2098eef4ea10e08ad62f474431e5a8d53930a7
mauerv <maurovelazquez1@gmail.com>
2019-06-04 19:07:49 -0300

    Add property for lists to be draggable

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,6 +1,6 @@
 <template>
-  <div id="app" class='row'>
-    <div v-for='(list, index) in original_lists' class='col-4'>
+  <draggable v-model='lists' :options="{group: 'lists'}" class='row dragArea' @end='listMoved'>
+    <div v-for='(list, index) in lists' class='col-4'>
       <h6>{{ list.name }}</h6>
       <hr />
 
@@ -13,11 +13,13 @@
         <button v-on:click='submitMessages(list.id)' class='btn btn-secondary'>Add</button>
       </div>
     </div>
-  </div>
+  </draggable>
 </template>
 
 <script>
+import draggable from 'vuedraggable'
 export default {
+  components: { draggable },
   props: ['original_lists'],
   data: function() {
     return {
@@ -26,6 +28,17 @@ export default {
     }
   },
   methods: {
+    listMoved: function(event) {
+      var data = new FormData
+      data.append('list[position]', event.newIndex + 1)
+
+      Rails.ajax({
+        url: `/lists/${this.lists[event.newIndex].id}/move`,
+        type: 'PATCH',
+        data: data,
+        dataType: 'json',
+      })
+    },
     submitMessages: function(list_id) {
       var data = new FormData
       data.append('card[list_id]', list_id)
@@ -48,8 +61,7 @@ export default {
 </script>
 
 <style scoped>
-p {
-  font-size: 2em;
-  text-align: center;
+.dragArea {
+  min-height: 20px;
 }
 </style>
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   resources :cards
-  resources :lists
+  resources :lists do
+  	member do
+  		patch :move
+  	end
+  end
+
   root to: 'lists#index'
 end
